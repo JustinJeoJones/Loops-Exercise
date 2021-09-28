855427f0cc88217d3786d94d4b2e777b785dff42
Jeo <jeo961@gmail.com>
2021-09-28 14:24:19 -0400

    third exercise

--- LoopExercises/Program.cs
+++ LoopExercises/Program.cs
@@ -25,6 +25,13 @@ namespace LoopExercises
                     runProgram = false;
                 }
             }
+            //3
+            int sum = 0;
+            for(int i = 0;i<=5; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
