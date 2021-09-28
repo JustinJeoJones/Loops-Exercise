e93fc0dae3a0934f65ece2bcf6ac8df3b6162e68
Jeo <jeo961@gmail.com>
2021-09-28 14:07:40 -0400

    added exercise 2

--- LoopExercises/Program.cs
+++ LoopExercises/Program.cs
@@ -6,13 +6,24 @@ namespace LoopExercises
     {
         static void Main(string[] args)
         {
+            //1
             for(int i = 0; i<=100; i++)
             {
                 if(i % 2 == 0) //is even
                 {
                     Console.WriteLine(i);
                 }
-                
+            }
+            //2
+            bool runProgram = true;
+            while(runProgram)
+            {
+                Console.WriteLine("Do you want to continue looping? y/n");
+                string choice = Console.ReadLine();
+                if(choice == "n")
+                {
+                    runProgram = false;
+                }
             }
         }
     }
