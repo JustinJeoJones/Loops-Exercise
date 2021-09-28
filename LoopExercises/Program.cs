7ffbeb7a9938c814743625578eb7da64a8fb1ce3
Jeo <jeo961@gmail.com>
2021-09-28 13:46:15 -0400

    first commit

---
+++ LoopExercises/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LoopExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<=100; i++)
+            {
+                if(i % 2 == 0) //is even
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+        }
+    }
+}
